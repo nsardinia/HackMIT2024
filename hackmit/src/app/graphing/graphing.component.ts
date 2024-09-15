df5120929087f0327e7947f7c37d5816e7630d46
Keith Khadar <113483023+Keith-Khadar@users.noreply.github.com>
2024-09-15 05:15:18 -0400

    Update graphing.component.ts

--- hackmit/src/app/graphing/graphing.component.ts
+++ hackmit/src/app/graphing/graphing.component.ts
@@ -69,6 +69,7 @@ export class GraphingComponent implements OnInit, OnDestroy {
               showticklabels: false, // Hide the x-axis tick labels
             },
             autosize: true, // Enable autosize to make the graph responsive
+            displayModeBar: false, // Hide all Plotly tools
           }
         );
         resolve(); // Plotly loaded, resolve the Promise
